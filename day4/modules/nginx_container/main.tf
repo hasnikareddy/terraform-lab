5e582405e1306cb9ff7e327cd170033f8bdf2c0b
Hasnika <hasnikareddy10@gmail.com>
2026-03-18 23:56:56 +0530

    Add day4 - Terraform modules

---
+++ day4/modules/nginx_container/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "docker_image" "nginx" {
+  name         = var.image_name
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = var.container_name
+
+  ports {
+    internal = 80
+    external = var.container_port
+  }
+}
---
+++ day4/outputs.tf
@@ -0,0 +1,7 @@
+output "container_1_url" {
+  value = module.nginx_8081.container_url
+}
+
+output "container_2_url" {
+  value = module.nginx_8082.container_url
+}
